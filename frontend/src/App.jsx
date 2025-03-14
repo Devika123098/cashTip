4726286c396a1e9e57fdd6cceacb6cb2f751757f
suvanshenoy <suvanshenoy155@gmail.com>
2025-03-14 22:19:59 +0530

    fix(src) forgot to add AccountType.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home.jsx";
-// import AccountType from "./pages/AccountType.jsx";
+import AccountType from "./pages/AccountType.jsx";
 import CreditCard from "./pages/CreditCard.jsx";
 import BankComparison from "./components/BankComparison.jsx";
 import CurrencyCalculator from "./components/CurrencyCalculator.jsx";
@@ -13,7 +13,7 @@ const App = () => {
 			<Route path="/bank-comparison" element={<BankComparison />} />
 			{/*<Route path="/budgeting-tools" element={<BudgetingTools />} />*/}
 			<Route path="/currency-calculator" element={<CurrencyCalculator />} />
-			{/*<Route path="/account-type" element={<AccountType />} />*/}
+			<Route path="/account-type" element={<AccountType />} />
 			<Route path="/cc-type" element={<CreditCard />} />
 		</Routes>
 	);
