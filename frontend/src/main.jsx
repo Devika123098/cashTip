44ba5aa4177c25fe155d151e616566b33918f0a3
suvanshenoy <suvanshenoy155@gmail.com>
2025-03-14 22:35:00 +0530

    fix(src) remove unwanted imports from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
-// import AccountType from "./pages/AccountType.jsx";
-import CreditCard from "./pages/CreditCard.jsx";
-import BankComparison from "./components/BankComparison.jsx";
+import { BrowserRouter } from "react-router";
 
 const root = document.getElementById("root");
 
