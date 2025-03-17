0aec9af97ef9159cebc8b8de916393b807ebd84d
Yonehnavus <121305562+suvanshenoy@users.noreply.github.com>
2025-03-17 20:18:14 +0000

    fix(pages): remove the extra space at last of tailwind style

--- frontend/src/pages/AccountType.jsx
+++ frontend/src/pages/AccountType.jsx
@@ -14,12 +14,12 @@ const AccountType = () => {
             <div className='sm:flex grid justify-center sm:gap-40 gap-15 cursor-pointer'>
 
                 <div className='flex flex-col items-center w-60 h-70 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl gap-y-4 hover:scale-105'>
-                    <img src="ATM.png" alt="chequing" className='w-50 py-1 '></img>
+                    <img src="ATM.png" alt="chequing" className='w-50 py-1'/>
                     <button className='cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Chequing Accounts</button>
                 </div>
         
                 <div className='flex flex-col items-center w-60 h-70 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl gap-y-4 hover:scale-105'>
-                    <img src="Investment.png" alt="saving" className='w-50 py-1 '></img>
+                    <img src="Investment.png" alt="saving" className='w-50 py-1 '/>
                     <button className='cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Saving Accounts</button>
                 </div>
 
