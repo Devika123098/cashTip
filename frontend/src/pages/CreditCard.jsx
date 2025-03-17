8f648c2bd0d79ff82428acf5e406f0348aacaabf
Yonehnavus <121305562+suvanshenoy@users.noreply.github.com>
2025-03-17 20:19:30 +0000

    fix(pages): follow the react  component style for img tag

--- frontend/src/pages/CreditCard.jsx
+++ frontend/src/pages/CreditCard.jsx
@@ -14,22 +14,22 @@ const CreditCard= () => {
           <div className='grid grid-cols-2 sm:gap-16 gap-8 justify-center items-center cursor-pointer'>
         
             <div className='flex flex-col items-center sm:w-60 sm:h-60 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl  hover:scale-105'>
-                  <img src="Airplane.png" alt="airplane"className='w-50 '></img>
+                  <img src="Airplane.png" alt="airplane"className='w-50 '/>
                     <button className='cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Travel</button>
             </div>
      
               <div className='flex flex-col items-center sm:w-60 sm:h-60 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl  hover:scale-105'>
-                  <img src="Casback.png" alt="cashback"className='w-50'></img>
+                  <img src="Casback.png" alt="cashback"className='w-50'/>
                     <button className='cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Cash back</button>
-        </div>
+              </div>
         
         <div className='flex flex-col items-center sm:w-60 sm:h-60 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl sm:gap-y-3 gap-y-2 hover:scale-105'>
-                  <img src="Percent.png" alt="percent"className='sm:w-45  sm:py-1'></img>
+                  <img src="Percent.png" alt="percent"className='sm:w-45  sm:py-1'/>
                     <button className='cursor-pointer  text-transparent bg-clip-text bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Low Interest</button>
         </div>
         
         <div className='flex flex-col items-center sm:w-60 sm:h-60 rounded-lg border-gray-500 bg-[#D9D9D9] shadow-xl  hover:scale-105'>
-                  <img src="Academic.png" alt="academic"className='w-50'></img>
+                  <img src="Academic.png" alt="academic"className='w-50'/>
                     <button className='cursor-pointer bg-clip-text text-transparent bg-gradient-to-r from-[#5293FF] to-[#943CE1]' type="button">Student</button>
         </div>
         
