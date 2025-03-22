60e2c317dbe23dddeb464ccd721e1e84d66e9701
suvanshenoy <suvanshenoy155@gmail.com>
2025-03-22 22:09:18 +0530

    fix(src): format App.jsx and

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,19 @@ import AccountType from "./pages/AccountType.jsx";
 import CreditCard from "./pages/CreditCard.jsx";
 import BankComparison from "./components/bank-comparison/BankComparison.jsx";
 import CurrencyCalculator from "./components/currency-calculator/CurrencyCalculator.jsx";
-import StepOne from "./components/pop-up/StepOne.jsx"
-
+import Popup from "./components/pop-up/Popup.jsx";
 const App = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/bank-comparison" element={<BankComparison />} />
-			{/*<Route path="/budgeting-tools" element={<BudgetingTools />} />*/}
-			<Route path="/currency-calculator" element={<CurrencyCalculator />} />
-			<Route path="/account-type" element={<AccountType />} />
-			<Route path="/cc-type" element={<CreditCard />} />
-			<Route path="/pop-up" element={<StepOne/>}/>
-		</Routes>
-	);
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bank-comparison" element={<BankComparison />} />
+            {/*<Route path="/budgeting-tools" element={<BudgetingTools />} />*/}
+            <Route path="/currency-calculator" element={<CurrencyCalculator />} />
+            <Route path="/account-type" element={<AccountType />} />
+            <Route path="/cc-type" element={<CreditCard />} />
+            <Route path="/pop-up" element={<Popup />} />
+        </Routes>
+    );
 };
 
 export default App;
