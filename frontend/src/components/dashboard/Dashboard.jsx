f89bc3fa004a8870c08e90d1a05d68dba3bffc32
suvanshenoy <suvanshenoy155@gmail.com>
2025-03-27 17:43:41 +0530

    fix(dashboard): Add a navbar with a profile avatar and a navbar button profile avatar when clicked shows dropdown options like profile, settings navbar button when clicked shows dropdown options like home , about fix(dashboard/components): Add a categories property to TableRow component and make the tables hover when clicked, add a edit button on the tables

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,56 +1,101 @@
 import TableRow from "./components/TableRow.jsx";
 import useAnswerStore from "../../store/answerStore.jsx";
 import { useState } from "react";
+import { Link } from "react-router";
+
 const Dashboard = () => {
 	const answers = useAnswerStore((state) => state.answers);
 	const [assigned, setAssigned] = useState([]);
 	const [available, setAvailable] = useState([]);
 	return (
 		<>
-			<div className="drawer lg:drawer-open">
-				<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-				<div className="drawer-content flex flex-col justify-center items-center">
-					<div className="overflow-x-auto">
-						<table className="table">
-							{/* head */}
-							<thead>
-								<TableRow data={["Category", "Assigned", "Available"]} />
-							</thead>
-							<tbody className="flex flex-col">
-								{answers.map((answer, index) => (
-									<TableRow
-										key={index}
-										answers={answer}
-										assigned={assigned}
-										available={available}
-									/>
-								))}
-							</tbody>
-						</table>
+			<div className="navbar shadow-sm bg-[#2563EB]">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<div
+							tabIndex={0}
+							role="button"
+							className="btn btn-ghost btn-circle"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-8 w-8"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="white"
+							>
+								{" "}
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h16M4 18h7"
+								/>{" "}
+							</svg>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu  menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3  w-52 px-2 shadow"
+						>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+						</ul>
 					</div>
-					<label
-						htmlFor="my-drawer-2"
-						className="btn btn-primary drawer-button lg:hidden"
-					>
-						Open drawer
-					</label>
 				</div>
-				<div className="drawer-side">
-					<label
-						htmlFor="my-drawer-2"
-						aria-label="close sidebar"
-						className="drawer-overlay"
-					></label>
-					<ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-						{/* Sidebar content here */}
-						<li>
-							<a>Sidebar Item 1</a>
-						</li>
-						<li>
-							<a>Sidebar Item 2</a>
-						</li>
-					</ul>
+				<div className="navbar-center">
+					<Link className="capitalize text-white text-2xl font-bold" to="/">
+						dashboard
+					</Link>
 				</div>
+				<div className="navbar-end">
+					<div className="dropdown dropdown-end">
+						<div
+							tabIndex={0}
+							role="button"
+							className="btn btn-ghost btn-circle avatar"
+						>
+							<div className="w-10 rounded-full">
+								<img
+									alt="profile"
+									src="https://cdn-icons-png.flaticon.com/128/3237/3237472.png"
+								/>
+							</div>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-53 p-2 shadow"
+						>
+							<li>
+								<Link to="/profile">Profile</Link>
+							</li>
+							<li>
+								<Link to="/settings">Settings</Link>
+							</li>
+							<li>
+								<Link to="/">Sign out</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className="w-full max-w-3/4 bg-base-100 p-2">
+				<table className="table mt-20">
+					<TableRow />
+					{/*<tbody className="flex flex-col">
+						{answers.map((answer, index) => (
+							<TableRow
+								key={index}
+								answers={answer}
+								assigned={assigned}
+								available={available}
+							/>
+						))}
+					</tbody>*/}
+				</table>
 			</div>
 		</>
 	);
--- frontend/src/components/dashboard/components/TableRow.jsx
+++ frontend/src/components/dashboard/components/TableRow.jsx
@@ -1,37 +1,49 @@
 const TableRow = ({
-  key = 0,
-  answers = ["Bills", "Needs", "Wants"],
-  availableAmounts = [300, 200, 600],
-  assignedAmounts = [150, 240, 560],
+	key = 0,
+	categories = ["Category", "Assigned", "Available"],
+	answers = ["Bills", "Needs", "But", "Wants", "Some", "Money"],
+	assignedAmounts = [150, 240, 560, 0, 100, 200],
+	availableAmounts = [300, 200, 600, 0, 20, 20],
 }) => {
-  return (
-    <>
-      {answers.map((answer, index) => (
-        <tr 
-          key={`${key}-${index}`} 
-          className="border border-gray-500 hover:bg-gray-50"
-        >
-          <td className="border border-gray-500 px-4 py-2">
-            <input
-              type="checkbox"
-              defaultChecked
-              className="checkbox accent-blue-500"
-              aria-label={`Select ${answer}`}
-            />
-          </td>
-          <td className="border border-gray-500 px-4 py-2 text-left">
-            {answer}
-          </td>
-          <td className="border border-gray-500 px-4 py-2 text-right">
-            ${availableAmounts[index].toLocaleString()}
-          </td>
-          <td className="border border-gray-500 px-4 py-2 text-right">
-            ${assignedAmounts[index].toLocaleString()}
-          </td>
-        </tr>
-      ))}
-    </>
-  );
+	return (
+		<>
+			{categories.map((category, index) => (
+				<th
+					key={`${key}-${index}`}
+					className="w-2 shadow bg-[#2563EB] text-white"
+				>
+					<div className="flex gap-4 ">{category}</div>
+				</th>
+			))}
+			{answers.map((answer, index) => (
+				<tr key={`${key}-${index}`} className="border-gray-300">
+					<td className="w-2/5 hover:bg-gray-200 shadow">
+						<div className="flex gap-4">
+							<input
+								type="checkbox"
+								defaultChecked
+								className="checkbox accent-blue-500"
+								aria-label={`Select ${answer}`}
+							/>
+							{answer}
+						</div>
+					</td>
+					<td className="hover:bg-gray-200  shadow">
+						${assignedAmounts[index].toLocaleString()}
+						<button type="button" className="btn float-right h-6">
+							edit
+						</button>
+					</td>
+					<td className=" hover:bg-gray-200 shadow">
+						${availableAmounts[index].toLocaleString()}
+						<button type="button" className="btn float-right h-6">
+							edit
+						</button>
+					</td>
+				</tr>
+			))}
+		</>
+	);
 };
 
 export default TableRow;
